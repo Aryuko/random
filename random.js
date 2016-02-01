098ff403fc6ca48c9c6cd1547b3f1f08790e7160
Zalodu <peter.kjellen@gmail.com>
2016-02-01 17:33:00 +0100

    add basic spinning functionality
    resolves #5

--- random.js
+++ random.js
@@ -26,7 +26,7 @@ function isAllowed (index) {
 	}
 	return true;
 }
-
+/*
 function updateResult (student) {
 	$("#output").html('<a href="https://gits-15.sys.kth.se/INDA15/' + student + '-week-' + week + '" target="_blank">' + student + '</a>');
 	
@@ -38,6 +38,57 @@ function updateResult (student) {
 		$("#numberWrapper ul").append(html);
 	}
 	disallowedNames.push(student);
+}*/
+
+function updateResult (student) {	
+	spinner(student);
+	
+	var html = '<li>' + student + '</li>';
+	if(disallowedNames.length == 0) {
+		$("#numberWrapper ul").html(html);
+	}
+	else {
+		$("#numberWrapper ul").append(html);
+	}
+	disallowedNames.push(student);
+}
+
+function spinner (student) {
+	var spinList = [];
+	spinList = spinList.concat(presentStudents);	//basically spinList = presentStudents, but doesn't glitch
+	
+	spinList.splice(spinList.indexOf(student), 1);	//move selected student to end of list
+	spinList.push(student);
+	
+	spinList = spinList.concat(spinList);			//multiply the list to make it longer
+	spinList = spinList.concat(spinList);
+	
+	var savedIndex = spinList.length - 1;			//save index we will scroll to
+	
+	spinList = spinList.concat(spinList);			//add one more to make the list continue after the destination, making it look endless
+	
+	buildSpinner(spinList);
+	$("#output ul").removeClass("outputTransition");
+	
+	$("#output ul").css('top', 0 + 'px');
+	
+	var elementHeight = $("#output").height() + 1;	//+1 from border
+	var targetTop = - (savedIndex * elementHeight);
+	
+	$("#output ul").addClass("outputTransition");
+	$("#output ul").css('top', targetTop + 'px');
+}
+
+function buildSpinner (list) {
+	var selectedStudent = list[list.length - 1];				//selected student, at end of list
+	
+	var html = '';
+	for (var i = 0; i < list.length - 1; i++) {
+		html += '<li>' + list[i] + '</li>';
+	}
+	html += '<a href="https://gits-15.sys.kth.se/INDA15/' + selectedStudent + '-week-' + week + '" target="_blank">' + selectedStudent + '</a>';
+	
+	$("#output ul").html(html);
 }
 
 $(function () {
